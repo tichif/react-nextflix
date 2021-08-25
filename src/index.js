79152efbbc3fb814dba02f00fc0e1e9d7f29d13c
Dalzon Charles-Hébert <charlby5@gmail.com>
2021-08-24 22:02:48 -0400

    Create global style

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import GlobalStyles from './global-styles';
+import { GlobalStyles } from './global-styles';
 
 ReactDOM.render(
   <>
