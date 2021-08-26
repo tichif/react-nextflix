0539f9c56ee51a64bd761c1b69fc5409f92c464f
Dalzon Charles-Hébert <charlby5@gmail.com>
2021-08-25 20:20:21 -0400

    Add a JumbotronContainer

--- src/App.js
+++ src/App.js
@@ -1,22 +1,7 @@
-import Jumbotron from './components/jumbotron';
-import jumboData from './fixtures/jumbo.json';
+import { JumbotronContainer } from './containers/jumbotron';
 
 const App = () => {
-  return (
-    <div>
-      <Jumbotron.Container>
-        {jumboData.map((item) => (
-          <Jumbotron key={item.id} direction={item.direction}>
-            <Jumbotron.Pane>
-              <Jumbotron.Title>{item.title}</Jumbotron.Title>
-              <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
-            </Jumbotron.Pane>
-            <Jumbotron.Image src={item.image} alt={item.subTitle} />
-          </Jumbotron>
-        ))}
-      </Jumbotron.Container>
-    </div>
-  );
+  return <JumbotronContainer />;
 };
 
 export default App;
